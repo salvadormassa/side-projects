2e3fd8767392d541b8efa03f0812e63531e3f3e8
Salvador <salvadorjmassa@gmail.com>
2022-06-18 09:42:42 -0400

    Removed print statements from variables and added them directly to the printtext function. Other small tweaks.

--- twenty_one_card_trick.py
+++ twenty_one_card_trick.py
@@ -29,27 +29,22 @@ def clrscr():
         os.system('cls')
     except Exception:
         pass
-    print("\n")
+    print('\n')
     time.sleep(2)
 
 # General print function
-def printtext(x, y):
-    if x != 0:
-        for letter in x:
-            print(letter, end="", flush=True)
-            time.sleep(y)
+def printtext(text, wait, new_line):
+    for letter in text:
+        print(letter, end="", flush=True)
+        time.sleep(wait)
+    if new_line == 1:
+        print('')
 
-# Prints text in a (string)... format
-def iterate_dots(text):
-    time.sleep(.5)
-    printtext(text, .05)
-    time.sleep(.5)
-    print('.', end='', flush=True)
-    time.sleep(.5)
-    print('.', end='', flush=True)
-    time.sleep(.5)
-    print('.', flush=True)
-    time.sleep(.5)
+# Prints text followed by ellipsis
+def ellipsis(dots):
+    for i in range(dots):
+        print('.', end='', flush=True)
+        time.sleep(time)
 
 # Removes player column from group, randomly shuffles the remaining 2,
 # then re-inserts user column
@@ -89,8 +84,7 @@ def lay_down_columns(columns):
 clrscr()
 
 # Start of program
-intro = "\nThis program will guess your card!\n"
-printtext(intro, .05)
+printtext("\nThis program will guess your card!", .05, 1)
 time.sleep(2)
 
 # Grabs 21 random cards from the deck
@@ -103,18 +97,16 @@ print('--------------------')
 time.sleep(2)
 
 # Stage 1, player picks card, deck is shuffled
-pick_card = "Pick a card, memorize it, and shuffle the cards.\n"
-type_ok = "Type OK to shuffle cards: "
 shuffling = "Shuffling"
-printtext(pick_card, .05)
+printtext("Pick a card, memorize it, and shuffle the cards.", .05, 1)
 time.sleep(1)
 while True:
-    printtext(type_ok, .05)
+    printtext("Type OK to shuffle cards: ", .05, 0)
     user_input = input()
     if user_input.upper() != 'OK':
         continue
     break
-iterate_dots("Shuffling")
+printtext("Shuffling...", .1, 1)
 # Shuffle the 21 cards
 random.shuffle(random_21)
 
@@ -124,11 +116,9 @@ column_2 = random_21[7:14]
 column_3 = random_21[14:21]
 columns = [column_1, column_2, column_3]
 # Print 3 columns of cards, asks player to find their card in columns
-deal_columns = "Three columns of cards are layed out in front of you.\n"
-which_column = "Is your card in column 1, 2, or 3? "
-printtext(deal_columns, .05)
+printtext("Three columns of cards are layed out in front of you.", .05, 1)
 columns = lay_down_columns(columns)
-printtext(which_column, .05)
+printtext("Is your card in column 1, 2, or 3? ", .05, 0)
 while True:
     try:
         user_input = int(input())
@@ -137,11 +127,10 @@ while True:
         continue
 
 # Stage 3, regroup card with player selected column in the middle. Repeat
-merge_columns = "The cards are put back together and again layed out in front of you.\n"
 columns = shuffle_columns()
-printtext(merge_columns, .05)
+printtext("The cards are put back together and again layed out in front of you.", .05, 1)
 columns = lay_down_columns(columns)
-printtext(which_column, .05)
+printtext("Is your card in column 1, 2, or 3? ", .05, 0)
 while True:
     try:
         user_input = int(input())
@@ -150,11 +139,10 @@ while True:
         continue
 
 # Stage 4, Repeat
-last_time = "The cards are put back together and for a third time are dealt.\n"
 columns = shuffle_columns()
-printtext(last_time, .05)
+printtext("The cards are put back together and are dealt for a third time.", .05, 1)
 columns = lay_down_columns(columns)
-printtext(which_column, .05)
+printtext("Is your card in column 1, 2, or 3? ", .05, 0)
 while True:
     try:
         user_input = int(input())
@@ -165,16 +153,16 @@ while True:
 # Stage 5, columns are put back together and their card is revealed
 # Their card will be the 11th fron the 'top', or the 4th in the middle column
 columns = shuffle_columns()
-iterate_dots("Calculating")
-print(f'Is {columns[1][3]} your card? ', end='')
+printtext("Calculating...", .2, 1)
+printtext(f'Is {columns[1][3]} your card? ', .05, 0)
 while True:
     user_input = input()
     if user_input[:3].lower() == 'yes':
-        printtext("It's magic!!!", .1); time.sleep(2)
+        printtext("It's magic!!!", .1, 0); time.sleep(2)
         sys.exit()
     elif user_input[:2].lower() == 'no':
-        printtext("I hope you learned something here.", .05); time.sleep(2)
+        printtext("I hope you learned something here.", .05, 0); time.sleep(2)
         sys.exit()
     else:
-        print("Was that your card?")
+        printtext("Was that your card? ", .05, 0)
         continue
